3ca2adf268c3c948d8a13bf459553072d68e3088
guyfredw <guyfredw@gmail.com>
2020-12-18 22:43:55 -0500

    Index Post component
    Basic index post component which displays the posts on the webpage

--- src/api/posts.js
+++ src/api/posts.js
@@ -1,1 +1,12 @@
+import apiUrl from '../apiConfig'
+import axios from 'axios'
 
+export const indexPosts = (user) => {
+  return axios({
+    method: 'GET',
+    url: apiUrl + '/posts/',
+    headers: {
+      Authorization: `Token ${user.token}`
+    }
+  })
+}
