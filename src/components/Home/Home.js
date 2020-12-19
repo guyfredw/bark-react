69b7e2f79f13b0f8804e08798d13555b785418c9
guyfredw <guyfredw@gmail.com>
2020-12-18 21:46:47 -0500

    Created Home component

---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react'
+
+const Home = () => (
+  <Fragment>
+    <h1>Welcome to Bark!</h1>
+    <h2>About Bark</h2>
+    <p>
+      Bark is a social media application, where users are able to post about almost anything they desire and others will be able to do the same, look at their posts and comment.
+    </p>
+  </Fragment>
+)
+
+export default Home
